f90380b03a7fde641489958af09643e7a7f65bab
Stephan Sahm <Stephan.Sahm@gmx.de>
2016-02-19 22:46:33 +0100

    outsourced numpy dependency for use with pypy

--- schlichtanders/mywrappers.py
+++ schlichtanders/mywrappers.py
@@ -1,6 +1,5 @@
 from __future__ import division
 import collections
-import numpy
 
 
 class defaultdict(collections.defaultdict):
@@ -12,22 +11,3 @@ class defaultdict(collections.defaultdict):
 
 def str_list(l):
     return '[' + ','.join(map(str, l)) + ']'
-
-
-def mean_gen(values):
-    sums = next(values)
-    for n, xs in enumerate(values):
-        try:
-            sums = [x+s for x, s in zip(xs, sums)]
-        except:
-            sums += xs
-    try:
-        #needs __future__.division
-        return [s/(n+2) for s in sums]
-    except:
-        return sums/(n+2)
-
-def mean(iterable):
-    if hasattr(iterable, "next"):
-        return mean_gen(iterable)
-    return numpy.mean(iterable)
